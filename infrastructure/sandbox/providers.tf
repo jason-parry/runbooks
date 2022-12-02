bd2798ee70a6d19740ce76b99c06d7ec0c4c2513
Jason Parry <46546344+jason-parry@users.noreply.github.com>
2022-12-02 15:31:52 +0800

    Update providers.tf

--- infrastructure/sandbox/providers.tf
+++ infrastructure/sandbox/providers.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  alias                      = "SUB-JPSBX"
+  #alias                      = "SUB-JPSBX"
   skip_provider_registration = true
-  subscription_id            = "f016bccd-36f9-4589-aacb-c2b50df8b535"
+  #subscription_id            = "f016bccd-36f9-4589-aacb-c2b50df8b535"
   features {}
 }
